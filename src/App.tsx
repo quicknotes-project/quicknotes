105c54b9fa663ae1a5c5d18386773b47f7de0f0e
vancomm <peshekhonov.ip@gmail.com>
2022-11-22 06:48:40 +0300

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useEffect, useState } from 'react';
 import { Allotment } from 'allotment';
 import Markdown from './components/Markdown';
